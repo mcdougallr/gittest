2bb5dda4cc6af86634a13d7d66b60881e150f16c
gould <gould@Rob-XPS>
2018-09-15 10:07:53 -0400

    commit 2: update of main

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -5,6 +5,8 @@
  */
 package gittest;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gould
@@ -15,7 +17,9 @@ public class Gittest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner input = new Scanner(System.in);
+       String name = input.nextLine();
+       System.out.println(name);
     }
     
 }
